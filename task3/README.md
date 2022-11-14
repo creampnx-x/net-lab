879786894c74bb4a993684276f5c7bbdd47e0d96
creampnx_x <2270436024@qq.com>
2022-11-14 11:15:59 +0800

    docs and dest file

---
+++ task3/README.md
@@ -0,0 +1,6 @@
+# task3
+
+## 环境
+
+1. python 3.11.0
+2. windows 11
--- task3/client.py
+++ task3/client.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import random
+import time
 
 
 def main():
@@ -17,8 +18,8 @@ def main():
         target_port = int(input("target server port: "))
 
     relative_file_path = './file.txt'  # input("文件相对路径: ")
-    block_min_length = 1  # int(input("最小块长度(min=0): "))
-    block_max_length = 100  # int(input("最大块长度(max=512): "))
+    block_min_length = int(input("最小块长度(min=0): "))
+    block_max_length = int(input("最大块长度(max=512): "))
 
     if block_max_length < 1 or 512 < block_max_length:
         block_max_length = 512
@@ -36,10 +37,8 @@ def main():
     blocks = get_blocks(relative_file_path,
                         (block_min_length, block_max_length))
 
-    print(blocks)
 
     tcp_socket.connect((target_ip, target_port))
-    # tcp_socket.listen(1000)
 
     while True:
         '''initailization: [type(2 bytes), N(4 bytes)]'''
@@ -67,15 +66,13 @@ def send_data(blocks=[], tcp_socket=socket.socket()):
     '''send data: [type(2 bytes), length(4 bytes), data(${length} bytes)]'''
     reverse_blocks = []
     for block in blocks:
-        print(block)
         tcp_socket.send(
             b''.join([(3).to_bytes(2), (len(block)).to_bytes(4), str(block).encode()]))
         
         message = tcp_socket.recv(1024)
         type = int.from_bytes(message[0: 2])
         if type == 4:
-            reverse_blocks = [recv_data(message), *reverse_blocks]
-        print("data", message)    
+            reverse_blocks = [recv_data(message), *reverse_blocks] 
     return reverse_blocks
 
 
@@ -86,12 +83,12 @@ def recv_data(message=b''):
 
     length = int.from_bytes(message[2: 6])
     reverse_data = message[6: 6 + length].decode()
-    print("data" ,reverse_data)
     return reverse_data
 
 
 def write_to_file(reverse_blocks=[], file_path='./reverse-file.txt'):
     file = open(file_path, "w")
+    file.write(f'{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())} \n')
     for block in reverse_blocks:
         file.write(block)
 
